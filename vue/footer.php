960d3ce88c5c53271ae4ee973f90b3d76d4a0773
ThibaultCougrand <thibault.cougrand@gmail.com>
2019-04-01 21:33:09 +0200

    Mise en place du scss de la navbar et du footer

---
+++ vue/footer.php
@@ -0,0 +1,10 @@
+<footer>
+    <div class="icones">
+        <a href="#"><i class="fab fa-facebook fa-4x"></i></a>
+        <a href="#"><i class="fab fa-twitter-square fa-4x"></i></a>
+        <a href="#"><i class="fab fa-instagram fa-4x"></i></a>
+    </div>
+    <div class="mention">
+        <h2><a href="vue/mentions-legale.html">mentions légale</a></h2>
+    </div>
+</footer> 
--- vue/template.php
+++ vue/template.php
@@ -8,7 +8,7 @@
         <!--FONT AWESOME-->
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
         <!--FEUILLE CSS-->
-        <link href="../css/style.css" rel="stylesheet" type="text/css"/>
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
         <!--POLICE D'ECRITURE GOOGLEFONT-->
         <link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
     </head>
